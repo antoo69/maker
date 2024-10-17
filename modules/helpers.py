605080ba9489b6b9d41ac3a2b761f1d5d9279b23
antoo69 <antobarry69@gmail.com>
2024-10-18 00:33:31 +0700

    Update helpers.py

--- modules/helpers.py
+++ modules/helpers.py
@@ -8,8 +8,26 @@ def owner_only(func):
     @wraps(func)
     def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
         user = update.effective_user
-        if user.id != config.OWNER_ID:
-            update.message.reply_text("Anda tidak memiliki izin untuk menggunakan perintah ini.")
-            return
-        return func(update, context, *args, **kwargs)
+        if user and user.id == config.OWNER_ID:
+            return func(update, context, *args, **kwargs)
+        else:
+            if update.message:
+                update.message.reply_text("Anda tidak memiliki izin untuk menggunakan perintah ini.")
+            elif update.callback_query:
+                update.callback_query.answer("Anda tidak memiliki izin untuk menggunakan perintah ini.")
+    return wrapper
+
+def admin_only(func):
+    @wraps(func)
+    def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
+        user = update.effective_user
+        chat = update.effective_chat
+        if chat and user:
+            member = chat.get_member(user.id)
+            if member.status in ['administrator', 'creator']:
+                return func(update, context, *args, **kwargs)
+        if update.message:
+            update.message.reply_text("Perintah ini hanya dapat digunakan oleh admin.")
+        elif update.callback_query:
+            update.callback_query.answer("Perintah ini hanya dapat digunakan oleh admin.")
     return wrapper
