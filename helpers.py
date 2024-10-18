ecf88608e0ee51188a28df9a60612a65f575b7ac
antoo69 <antobarry69@gmail.com>
2024-10-18 09:40:00 +0700

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from functools import wraps
+import config
+
+def owner_only(func):
+    @wraps(func)
+    def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == config.OWNER_ID:
+            return func(update, context, *args, **kwargs)
+        else:
+            update.message.reply_text("Anda tidak memiliki izin untuk menggunakan perintah ini.")
+    return wrapper
