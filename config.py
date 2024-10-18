5fe214ab3c584557b2b167379191f1b3e325230f
antoo69 <antobarry69@gmail.com>
2024-10-18 09:40:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,2 @@
-import os
-from dotenv import load_dotenv
-
-# Memuat variabel lingkungan dari file .env
-load_dotenv(".env")
-
-# Mengambil nilai TOKEN dari variabel lingkungan
-TOKEN = os.getenv('TOKEN')
-
-# Mengambil nilai OWNER_ID dari variabel lingkungan dan mengonversinya ke integer
-OWNER_ID = int(os.getenv('OWNER_ID'))
-
-# Menambahkan variabel konfigurasi baru
-DATABASE_FILE = 'data/subscriptions.db'
-BLACKLIST_USERS_FILE = 'data/blacklist_users.json'
-BLACKLIST_WORDS_FILE = 'data/blacklist_words.json'
-MUTED_USERS_FILE = 'data/muted_users.json'
-
-
-# Menambahkan konfigurasi untuk zona waktu
-TIMEZONE = 'Asia/Jakarta'
-
-# Menambahkan konfigurasi untuk durasi subscription default (dalam hari)
-DEFAULT_SUBSCRIPTION_DURATION = 30
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+OWNER_ID = 123456789  # Ganti dengan ID Telegram owner
