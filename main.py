28b2e357040f5a407148322086076c9026b7aeaa
antoo69 <antobarry69@gmail.com>
2024-10-24 22:11:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from modules import setup_all_handlers
 import config
 
 def main():
+    """
+    Fungsi utama untuk menjalankan bot Telegram.
+    """
     # Buat updater dan dispatcher menggunakan token bot dari config.py
     updater = Updater(token=config.BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
+    dispatcher = updater.dispatcher
 
     # Panggil fungsi setup_all_handlers dari modules/__init__.py
-    setup_all_handlers(dp)
+    setup_all_handlers(dispatcher)
 
     # Mulai polling untuk menerima pesan dari Telegram
     updater.start_polling()
